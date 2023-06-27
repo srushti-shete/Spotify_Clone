b176254bac1f3830e2e3d5e8817a16b737b82369
Srushti Shete <shetesrushti11@gmail.com>
2023-06-27 16:40:10 +0530

    Updated code for login page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import Login from "./components/Login";
 import Spotify from "./components/Spotify";
 import { reducerCases } from "./utils/Constants";
 import { useStateProvider } from "./utils/StateProvider";
-
 export default function App() {
-  const stateProvider = useStateProvider();
-  
+  const [{ token }, dispatch] = useStateProvider();
   useEffect(() => {
     const hash = window.location.hash;
     if (hash) {
       const token = hash.substring(1).split("&")[0].split("=")[1];
       if (token) {
-        stateProvider.dispatch({ type: reducerCases.SET_TOKEN, token });
+        dispatch({ type: reducerCases.SET_TOKEN, token });
       }
     }
     document.title = "Spotify";
-  }, [stateProvider.dispatch]);
-  
-  const { token } = stateProvider;
-  
+  }, [dispatch, token]);
   return <div>{token ? <Spotify /> : <Login />}</div>;
 }
