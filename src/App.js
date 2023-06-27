746004c5a28d7e3cb695da475f9fcd2a5ccd5bf4
Srushti Shete <shetesrushti11@gmail.com>
2023-06-27 16:25:09 +0530

    Completed Login and get Access token from spotify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Login from "./components/Login";
+import Spotify from "./components/Spotify";
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const stateProvider = useStateProvider();
+  
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      if (token) {
+        stateProvider.dispatch({ type: reducerCases.SET_TOKEN, token });
+      }
+    }
+    document.title = "Spotify";
+  }, [stateProvider.dispatch]);
+  
+  const { token } = stateProvider;
+  
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
-
-export default App;
