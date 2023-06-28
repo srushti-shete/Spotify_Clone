22c0a9518183599c77cb0043b8749608d7872be9
Srushti Shete <shetesrushti11@gmail.com>
2023-06-28 16:00:04 +0530

    Completed & Designed the body of the Spotify

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef, useState, usrState} from 'react';
 import styled from "styled-components";
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
@@ -9,6 +9,17 @@ import { useStateProvider } from "../utils/StateProvider";
 import { reducerCases } from "../utils/Constants";
 export default function Spotify() {
   const [{ token }, dispatch] = useStateProvider();
+  const [navBackground, setNavBackground] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState(false);
+  const bodyRef = useRef();
+  const bodyScrolled = () => {
+    bodyRef.current.scrollTop >= 30
+      ? setNavBackground(true)
+      : setNavBackground(false);
+    bodyRef.current.scrollTop >= 268
+      ? setHeaderBackground(true)
+      : setHeaderBackground(false);
+  };
   useEffect(() => {
     const getUserInfo = async () => {
       const { data } = await axios.get("https://api.spotify.com/v1/me", {
@@ -30,10 +41,10 @@ export default function Spotify() {
     <Container>
       <div className='spotify__body'>
         <Sidebar/>
-        <div className='body'>
-          <Navbar/>
-          <div className='body__contents'>
-            <Body/>
+        <div className="body" ref={bodyRef} onScroll={bodyScrolled}>
+          <Navbar navBackground={navBackground} />
+          <div className="body__contents">
+            <Body headerBackground={headerBackground} />
           </div>
         </div>
       </div>
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -4,6 +4,8 @@ export const initialState = {
   token: null,
   playlists: [],
   userInfo: null,
+  selectedPlaylistId: "5BWIXFc6fUizmC2Mmihll5",
+  selectedPlaylist: null,
 };
 
 const reducer = (state, action) => {
@@ -22,6 +24,16 @@ const reducer = (state, action) => {
       return {
         ...state,
         userInfo: action.userInfo,
+      };
+      case reducerCases.SET_PLAYLISTS:
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+      case reducerCases.SET_PLAYLIST:
+      return {
+        ...state,
+        selectedPlaylist: action.selectedPlaylist,
       };
     default:
       return state;
