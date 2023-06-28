75af70c9ef318690bf3d45e4d4efffca0449afb3
Srushti Shete <shetesrushti11@gmail.com>
2023-06-28 15:30:18 +0530

    Completed Navbar with displaying UserInfo

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from "styled-components";
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import Body from './Body';
 import Footer from './Footer';
+import axios from "axios";
+import { useStateProvider } from "../utils/StateProvider";
+import { reducerCases } from "../utils/Constants";
 export default function Spotify() {
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const { data } = await axios.get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+      const userInfo = {
+        userId: data.id,
+        userUrl: data.external_urls.spotify,
+        name: data.display_name,
+      };
+      dispatch({ type: reducerCases.SET_USER, userInfo });
+    };
+    getUserInfo();
+  }, [dispatch, token]);
   return (
     <Container>
       <div className='spotify__body'>
